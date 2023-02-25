987379b9b3931567d341e4da2dff6754c5b1236f
Daniel Antunes Ribeiro <d.a_ribeiro2@hotmail.com>
2023-02-25 18:31:06 -0400

    Criando estrutura de arquivos e pastas

---
+++ backend/src/db/dbConfig.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongose");
+
+const databaseUrl =
+  "mongodb+srv://admin:YzOCE9cR7TOWpyCV@cluster0.nxhs41w.mongodb.net/";
+
+const conexaoDb = () => {
+  return mongoose
+    .connect(databaseUrl)
+    .then(() => console.log("Mongo conectado com sucesso!"))
+    .catch((error) => console.log("Erro de conexão com o banco.\n", error));
+};
+
+const objetoValido = (id) => {
+  return mongoose.Types.ObjectId.isValid(id);
+};
+
+module.exports = {
+  conexaoDb,
+  objetoValido,
+};
---
+++ backend/src/index.js
@@ -0,0 +1,4 @@
+const express = require("express");
+const cors = require("cors");
+const { connectToDatabase } = require("./db/dbConfig");
+
