461b78d2027c5f09cfe411916bdb884bf3eb7d69
Daniel Antunes Ribeiro <d.a_ribeiro2@hotmail.com>
2023-02-28 20:32:43 -0400

    implementando create categoria

--- frontend/src/components/Categorias/Categorias.jsx
+++ frontend/src/components/Categorias/Categorias.jsx
@@ -2,14 +2,16 @@ import "./Categorias.css";
 import { Api } from "../../api/api";
 import { useState } from "react";
 import { useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faCirclePlus } from "@fortawesome/free-solid-svg-icons"
 
 export function Categorias() {
   const [categorias, setCategorias] = useState();
 
   async function relizarRequisicao() {
-    const buscarCategorias = Api.categorias.todasCategorias();
-    const resposta = await Api.buildApiGetRequest(buscarCategorias);
-    const resultado = await resposta.json();
+    const categoriasDoBanco = Api.categorias.todasCategorias();
+    const categoriasCarregadas = await Api.buildApiGetRequest(categoriasDoBanco);
+    const resultado = await categoriasCarregadas.json();
 
     setCategorias(resultado);
   }
@@ -26,15 +28,45 @@ export function Categorias() {
     );
   }
 
+  async function novaCateoria(event){
+    event.preventDefault()
+    const nome = event.target.nomeCategoria.value
+    const objeto = {nome}
+    
+    const criandoUrl = Api.categorias.criar()
+    const resposta = await Api.buildApiPostRequest(criandoUrl, objeto)
+    const resultado = await resposta.json()
+    console.log(resultado)
+    if (resposta.status === 201) {
+      alert("Categoria criada com sucesso, parabéns! ;-)")
+    } else {
+      alert(resultado.message)
+    }
+
+  }
+
   return (
     <div className="categorias">
-      <div className="entradaCategoria">
+      {/*<div className="entradaCategoria">
         <input
           type="text"
           id="entradaCategoria"
           placeholder="Informe uma categoria"
         />
-      </div>
+        <a href="#" className="novaCateoria" onClick={novaCateoria}>
+          <FontAwesomeIcon icon= { faCirclePlus } size="xl" />
+        </a>
+      </div>*/}
+      <form onSubmit={novaCateoria}>
+        <div className="entradaCategoria">
+          <input 
+            type="text"
+            id="nomeCategoria"
+            placeholder="Informe uma categoria"
+          />
+          <input type="submit" value="Adicionar" />
+        </div>
+      </form>
       {categorias.map((categoria) => (
         <div className="categoria" key={categoria._id}>
           <span>{categoria.nome}</span>
