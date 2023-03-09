ce3bfa9149626b7e02231caf93ce037ddaacc061
Daniel Antunes Ribeiro <d.a_ribeiro2@hotmail.com>
2023-03-08 21:44:33 -0400

    Reconfigurando useNavigate

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,26 +1,39 @@
-import "./Header.css"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faCirclePlus } from "@fortawesome/free-solid-svg-icons"
-import { faList } from "@fortawesome/free-solid-svg-icons"
+import "./Header.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCirclePlus } from "@fortawesome/free-solid-svg-icons";
+import { faList } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate } from "react-router-dom";
 
-function Header () {
-    return (
-        <nav>
-            <div className="conteudoNav">
-                <div className="logo">
-                    <a href="/">
-                        <h1>Cards Aleatórios</h1>
-                    </a>
-                </div>
-                <div className="botoes">
-                    <a href="/novoCard">Novo card &nbsp;
-                    <FontAwesomeIcon icon= { faCirclePlus } /></a>
-                    <a href="/categorias">Categorias &nbsp;
-                    <FontAwesomeIcon icon= { faList } /></a>
-                </div>
-            </div>
-        </nav>
-    )
+function Header() {
+  const navegar = useNavigate();
+  function novoCard() {
+    navegar("/novoCard");
+  }
+  function categorias() {
+    navegar("/categorias");
+  }
+
+  return (
+    <nav>
+      <div className="conteudoNav">
+        <div className="logo">
+          <a href="/">
+            <h1>Cards Aleatórios</h1>
+          </a>
+        </div>
+        <div className="botoes">
+          <button onClick={novoCard} id="bntNovoCard">
+            Novo card &nbsp;
+            <FontAwesomeIcon icon={faCirclePlus} />
+          </button>
+          <button onClick={categorias} id="bntCategorias">
+            Categorias &nbsp;
+            <FontAwesomeIcon icon={faList} />
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
-export default Header
+export default Header;
